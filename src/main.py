83fad11a127520d2f707f4a7147ce78d6577ecd0
HonzikCZI <jan.sara.cze@gmail.com>
2024-08-10 10:29:56 +0200

    obrazek

--- src/main.py
+++ src/main.py
@@ -6,7 +6,19 @@ pygame.init()
 # obrazovka
 width = 1000
 height = 600
-screen = pygame
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("RALLY")
+
+# hlavni cyklus
+lets_continue = True
+while lets_continue:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            lets_continue = False
+            
+            
+    # update obrazovky
+    pygame.display.update()
 
 # ukonceni hry
 pygame.quit
