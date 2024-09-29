3ec7ffe01b7fd9260205a37043e7d4a1f720cd16
HonzikCZI <jan.sara.cze@gmail.com>
2024-09-29 08:35:43 +0200

    příprava textu

--- src/main.py
+++ src/main.py
@@ -5,9 +5,9 @@ import random
 pygame.init()
 
 # obrazovka
-width = 1000
-height = 600
-screen = pygame.display.set_mode((width, height))
+screen_width = 1280
+screen_height = 720
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("RALLY")
 
 # nastaveni hry
@@ -17,6 +17,7 @@ player_start_lives = 5
 car_start_speed = 2
 car_speed_acceleration = 0.5
 speed = 0
+score = 0
 
 player_lives = player_start_lives
 car_speed = car_start_speed
@@ -24,13 +25,39 @@ car_speed = car_start_speed
 car_x = random.choice([-1, 1])
 car_y = random.choice([-1, 1])
 
+# barvy
+black = (0, 0, 0)
+
+# fonty
+big_font = pygame.font.Font("fonts/Emulogic.ttf", 50)
+#middle_font = pygame.font.Font("font/Emulogic.ttf", 30)
+
+# obrazky
+background_image = pygame.image.load("img/silnice.png")
+background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
+background_image_rect = background_image.get_rect()
+background_image_rect.topleft = (0, 0)
+
+car_image = pygame.image.load("img/wrc.png")
+car_image = pygame.transform.scale(car_image, (300,100))
+car_image_rect = car_image.get_rect()
+car_image_rect.center = (screen_width//2, screen_height//2)
+
+# texty
+#score_text = middle_font.render(f"score: {score}",True, black)
+#score_text_rect = score_text.get_rect()
+
+
 # hlavni cyklus
 lets_continue = True
 while lets_continue:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             lets_continue = False        
-            
+    
+    # obrazky
+    screen.blit(background_image, background_image_rect)
+    screen.blit(car_image, car_image_rect)
             
     # update obrazovky
     pygame.display.update()
